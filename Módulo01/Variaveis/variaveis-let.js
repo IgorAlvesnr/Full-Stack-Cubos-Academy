0d048ac8c42c3058fded9104acda99c6d892890e
IgorALves <igorianrgit@gmail.com>
2024-01-24 20:43:03 -0300

    Aula de Variáveis 24/01/2024.

--- Módulo01/Variaveis/variaveis-let.js
+++ Módulo01/Variaveis/variaveis-let.js
@@ -6,4 +6,7 @@ primeiroNome = "Pedro";
 
 console.log(primeiroNome);
 
-// let: permite alteração da variável durante a construção do código e todas alterações serão guardadas.
+/*- let: permite alteração da variável durante a construção do código e todas alterações serão guardadas.
+
+- Não escreva tente escrever "let" novamente para mudar a variável primeiroNome pois dará erro. Pois parecerá
+que você está tentando criar uma nova variavel.*/
