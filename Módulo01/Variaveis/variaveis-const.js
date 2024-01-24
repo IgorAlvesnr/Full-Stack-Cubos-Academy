7e1db194024f0164b9149a85c187009c8cb79562
IgorALves <igorianrgit@gmail.com>
2024-01-24 19:58:50 -0300

    Aula de Variáveis 24/01/2024

---
+++ Módulo01/Variaveis/variaveis-const.js
@@ -0,0 +1,6 @@
+const primeiroNome = "José";
+
+console.log(primeiroNome);
+
+// const: significa que não permite alteração da variável. É para ser usada quando soubermos que não alteraremos
+// a variável durante a construção do código.
---
+++ Módulo01/Variaveis/variaveis-let.js
@@ -0,0 +1,9 @@
+let primeiroNome = "Messias";
+
+console.log(primeiroNome);
+
+primeiroNome = "Pedro";
+
+console.log(primeiroNome);
+
+// let: permite alteração da variável durante a construção do código e todas alterações serão guardadas.
