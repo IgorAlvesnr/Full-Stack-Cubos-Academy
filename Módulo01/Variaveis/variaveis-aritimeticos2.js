9622d679d8368ebd4a38bf8ac918ead1768eeeeb
IgorALves <igorianrgit@gmail.com>
2024-01-25 15:55:23 -0300

    Aula de Variáveis 24/01/2024.

---
+++ Módulo01/Variaveis/variaveis-aritimeticos2.js
@@ -0,0 +1,17 @@
+let x = 12;
+
+// x = x + 3; --> resultado 15.
+// x += 3; --> resultado também 15, += INCREMENTE.
+
+// x = x - 3; --> resultado 9.
+// x -= 3; --> resultado também 9, -= INCREMENTE.
+
+// x = x * 3; --> resultado 36.
+// x *= 3; --> resultado também 36, *= INCREMENTE.
+
+// x = x / 3; --> resultado 4.
+// x /= 3; --> resultado tabém 4, /= INCREMENTE.
+
+console.log(x);
+
+// ou seja para INCREMENTAR o valor a variável use o sinal aritimético antes do sinal de igual(recebe).
