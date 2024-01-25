1bd18b337b296c455115abe25619caa4506a5cec
IgorALves <igorianrgit@gmail.com>
2024-01-24 22:08:44 -0300

    Aula de variáveis 24/01/2024

---
+++ Módulo01/Variaveis/variaveis-aritimeticos.js
@@ -0,0 +1,13 @@
+let x = 10, y = 2;
+
+let resultado = x + y; //SOMA
+console.log(resultado); //12
+
+resultado = x - y; //SUBTRAÇÃO
+console.log(resultado); //8
+
+resultado = x * y; //MULTIPLICAÇÃO
+console.log(resultado); //20
+
+resultado = x / y; //DIVISÃO
+console.log(resultado); //5
---
+++ Módulo01/Variaveis/variaveis-tipos.js
@@ -0,0 +1,19 @@
+let nome = "Igor" //Strig ou seja texto
+console.log(nome);
+let idade = 30; //number inteiro
+console.log(idade);
+let altura = 1.77 //number decimal
+console.log(altura);
+let nota;//quando não atribuí valor deixa indefinido
+console.log(nota);
+nota = 10;//agora atribuí valor a nota por isso aparecerá
+console.log(nota);
+nota = null;// null: anula o valor de nota aparecendo null.
+console.log(nota);
+
+/*Existe ainda: 
+Boolean: Abtribui valor lógico a uma condicional; True ou False
+Object:  Multiplos pares de chaves-valor, formando um conjunto de propriedades
+Symbol: tipo de dado ÚNICO e IMUTÁVEL, geralmente rtilizados para criar identificadores únicos.
+
+esses são os 7 tipos de dados primários*/
