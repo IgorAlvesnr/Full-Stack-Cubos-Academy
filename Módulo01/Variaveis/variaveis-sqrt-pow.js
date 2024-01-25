7a4574ec97347895dd66724a91cbbe882bf5bf01
IgorALves <igorianrgit@gmail.com>
2024-01-25 16:42:27 -0300

    Aula de Variáveis 24/01/2024.

---
+++ Módulo01/Variaveis/variaveis-sqrt-pow.js
@@ -0,0 +1,6 @@
+let raiz = Math.sqrt(9);// forma de calcular raiz quadrada.
+console.log(raiz);
+
+let potencia = Math.pow(2, 3); // onde o primeiro número é o BASE e o segundo e o EXPOENTE.
+// potencia = 2 ** 3 ; outra forma de calcular potência.
+console.log(potencia);
