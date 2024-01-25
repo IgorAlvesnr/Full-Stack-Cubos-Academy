911a8e6ce4837168ae5da72c0c5938b4d4bcb80f
IgorALves <igorianrgit@gmail.com>
2024-01-24 22:28:35 -0300

    Aula de variáveis 24/01/2024.

---
+++ Módulo01/Variaveis/variaveis-concatenar.js
@@ -0,0 +1,11 @@
+let nome = "Igor";
+let sobrenome = "Alves";
+let nomeCompleto = nome + " " + sobrenome;//utiliza o "+" depois e antes das strings que quer juntar.
+console.log(nomeCompleto);
+
+let produto = "Livro";
+let preço = 50;
+let mensagem = `O ${produto} custa R$ ${preço},00.`;
+console.log(mensagem);
+
+// no segundo exemplo utilizasse o concatenar tamplates strings que utilizasse o formado de crases(``).
