1f3f2191ce6af7e640db4a4b6d59a8c984a6f412
Igor Alves <157431285+IgorAlvesnr@users.noreply.github.com>
2024-01-25 17:11:48 -0300

    Update IMC.js

--- Módulo01/Exercícios de Aula/Exercicio-de-variaveis/IMC.js
+++ Módulo01/Exercícios de Aula/Exercicio-de-variaveis/IMC.js
@@ -1,5 +1,5 @@
 let nome = "Igor";
 let altura = 1.77;
 let peso = 93
-let imc = (peso / (altura ** 2)).toFixed(2);
+let imc = (peso / (altura ** 2)).toFixed(2); //usei o toFixed(x) para limitar a quantidade de casas decimais.
 console.log(`Olá ${nome}, seu IMC é ${imc}`);
