eb6d73cbae3083a5411d4f5a79489099ada659a1
IgorALves <igorianrgit@gmail.com>
2024-01-24 21:19:40 -0300

    Aula de Variáveis 24/01/2024.

---
+++ Módulo01/Variaveis/variaveis-escopo.js
@@ -0,0 +1,12 @@
+{
+    const idade = 30;
+    {
+        console.log(idade);
+    }
+}
+
+console.log(idade);
+
+/*ESCOPO ou seja { }: se refere a região do código onde uma variável pode ser acessada. 
+Variaveis criadas dentro do ESCOPO só podem ser lidas se estivem dentro do ESCOPO, se não estiver dentro
+dará erro e aparecerá ReferenceError: idade is not defined"*/
