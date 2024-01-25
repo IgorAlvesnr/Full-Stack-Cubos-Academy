1d56242650a09bbd44f7c59281fa2f1eb7c47d80
IgorALves <igorianrgit@gmail.com>
2024-01-25 16:26:41 -0300

    Aula de Variáveis 24/01/2024.

--- Módulo01/Variaveis/variaveis-aritimeticos2.js
+++ Módulo01/Variaveis/variaveis-aritimeticos2.js
@@ -1,10 +1,12 @@
 let x = 12;
 
 // x = x + 3; --> resultado 15.
-// x += 3; --> resultado também 15, += INCREMENTE.
+// x += 3; --> resultado também 15, += INCREMENTE a quantidade pedida ao valor de x.
+// x++; resultado 13, INCREMENTA apenas 1 ao resultado de x.
 
 // x = x - 3; --> resultado 9.
-// x -= 3; --> resultado também 9, -= INCREMENTE.
+// x -= 3; --> resultado também 9, -= DECREMENTA a quantidade pedida ao valor de x.
+// x--; resuldade 11, x-- DECREMENTA apenas 1 ao resultado de x.
 
 // x = x * 3; --> resultado 36.
 // x *= 3; --> resultado também 36, *= INCREMENTE.
@@ -14,4 +16,6 @@ let x = 12;
 
 console.log(x);
 
-// ou seja para INCREMENTAR o valor a variável use o sinal aritimético antes do sinal de igual(recebe).
+// ou seja para INCREMENTAR ou DECREMENTAR o valor à variável use o sinal aritimético antes do sinal de igual(recebe).
+// não podemos utilizar x** pois esse símbolo é de EXPONENCIAÇÃO.
+// não podemos usar o // pois esse símbolo permite adicionarmos comentários no meio do código.
