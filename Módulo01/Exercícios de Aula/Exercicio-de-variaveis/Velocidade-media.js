116e22a8262bacb01248c693d60ad0b461c75ffa
IgorALves <igorianrgit@gmail.com>
2024-01-25 20:41:54 -0300

    Exercício de Variáveis.

---
+++ Módulo01/Exercícios de Aula/Exercicio-de-variaveis/Velocidade-media.js
@@ -0,0 +1,9 @@
+//Um trem percorreu 453 Km em 6h, Qual a Velocidade média percorrida?
+
+let distancia = 453;
+let tempo = 6;
+let velocidadeMedia = ((distancia / tempo).toFixed(1));
+console.log(`A velocidade média foi de ${velocidadeMedia} km/h`);
+
+/*Lembrar de colocar a DISTÂNCIA e o TEMPO na mesma unidade de médida. 
+Haja vista que Velociade no Brasil usa-se por convenção Km/h.*/
