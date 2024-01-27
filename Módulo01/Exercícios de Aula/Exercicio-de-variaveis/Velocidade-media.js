c23f0176aad1d9de5a6ecbcf613706a173174a75
IgorALves <igorianrgit@gmail.com>
2024-01-26 22:07:42 -0300

    Total de 13 exercícios de VARIÁVEIS.

--- Módulo01/Exercícios de Aula/Exercicio-de-variaveis/Velocidade-media.js
+++ Módulo01/Exercícios de Aula/Exercicio-de-variaveis/Velocidade-media.js
@@ -5,5 +5,5 @@ let tempo = 6;
 let velocidadeMedia = ((distancia / tempo).toFixed(1));
 console.log(`A velocidade média foi de ${velocidadeMedia} km/h`);
 
-/*Lembrar de colocar a DISTÂNCIA e o TEMPO na mesma unidade de médida. 
+/*Lembrar de colocar a distância e o TEMPO na mesma unidade de médida. 
 Haja vista que Velociade no Brasil usa-se por convenção Km/h.*/
