deea93022a43de45fa723d8f23930d1b6430eb21
IgorALves <igorianrgit@gmail.com>
2024-01-26 21:07:23 -0300

    Exercícios de Variáveis.

---
+++ Módulo01/Exercícios de Aula/Exercicio-de-variaveis/Angulos-internos.js
@@ -0,0 +1,4 @@
+let numeroDeLados = 7;
+let somaAngulosInternos = (numeroDeLados - 2) * 180;
+
+console.log(`A soma dos angulos internos de um poligono de ${numeroDeLados} lados é ${somaAngulosInternos}.`);
