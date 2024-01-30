d6af3a8bf08096e908b6177fc5252e3d22166e0d
IgorALves <igorianrgit@gmail.com>
2024-01-30 17:22:13 -0300

    Aula de Condicionais.

--- Módulo01/Condicionais/Exercicio-de-aula.js
+++ Módulo01/Condicionais/Exercicio-de-aula.js
@@ -1,11 +1,22 @@
 const valorDaCompra = 100;
-const numeroDeParcelas = 3;
+const numeroDeParcelas = 7;
 const desconto = valorDaCompra * 0.9;
 
 
 if (numeroDeParcelas === 1) {
     console.log(`Você deve pagar ${desconto} reais`);
 } else {
-    const valorFinal = (valorDaCompra / numeroDeParcelas).toFixed(2);
-    console.log(`Você deve pagar ${numeroDeParcelas} parcelas de R$${valorFinal} reais`);
+    if (numeroDeParcelas >= 2 && numeroDeParcelas <= 6) {
+
+        const valorFinal = (valorDaCompra / numeroDeParcelas).toFixed(2);
+        console.log(`Você deve pagar ${numeroDeParcelas} parcelas sem juros de R$${valorFinal} reais.`);
+
+    } else
+        if (numeroDeParcelas >= 7 && numeroDeParcelas <= 12) {
+            const valorFinal = (valorDaCompra * (1 + 1 / 100) ** numeroDeParcelas).toFixed(2);
+            const valorJuros = (valorFinal / numeroDeParcelas).toFixed(2);
+            console.log(`Você deve pagar ${numeroDeParcelas}x de R$${valorJuros} com o Total da compra em R$${valorFinal} reais, Devido a incidência de juros.`)
+        } else {
+            console.log("Número de parcelas invalidos.");
+        }
 }
