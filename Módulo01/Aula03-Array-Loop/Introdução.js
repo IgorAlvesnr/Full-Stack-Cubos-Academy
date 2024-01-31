534d311bbbf06e5d89e2302018a40909b51ef174
IgorALves <igorianrgit@gmail.com>
2024-01-31 19:14:32 -0300

    Aula de ARRAY e LOOPs

---
+++ Módulo01/Aula03-Array-Loop/Introdução.js
@@ -0,0 +1,9 @@
+const nomeDeEstudantes = ['jose', 'joão', 'maria', 'joana', 'andre'];
+
+console.log(nomeDeEstudantes); //Todos os nomes
+console.log(nomeDeEstudantes[0]);//jose
+console.log(nomeDeEstudantes[1]);//joão
+console.log(nomeDeEstudantes[2]);//maria
+console.log(nomeDeEstudantes[3]);//joana
+console.log(nomeDeEstudantes[4]);//andre
+console.log(nomeDeEstudantes[5]);//undefined pq não tem nome no index 5.
