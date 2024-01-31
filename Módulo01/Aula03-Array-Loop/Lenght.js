12cc55da5bfe63ba19284c0cabe978dc1af91f0a
IgorALves <igorianrgit@gmail.com>
2024-01-31 19:42:13 -0300

    Aula de ARRAY e LOOPs

---
+++ Módulo01/Aula03-Array-Loop/Lenght.js
@@ -0,0 +1,5 @@
+const nomesDeEstudantes = ['jose', 'joao', 'maria', 'joana', 'andre'];
+console.log(nomesDeEstudantes.length);//imprime o tamanho do array
+
+nomesDeEstudantes[nomesDeEstudantes.length] = 'pedro' //incluí o nome "pedro" no final do array consequentemente aumentando o tamanho do array
+console.log(nomesDeEstudantes);
