ae9df71d7514bb1e86613b93ca1fd4b4d9592fef
IgorALves <igorianrgit@gmail.com>
2024-01-31 19:59:05 -0300

    Aula de ARRAY e LOOPs

---
+++ Módulo01/Aula03-Array-Loop/Push-pop.js
@@ -0,0 +1,7 @@
+const nomesDeEstudantes = ['jose', 'joao', 'maria', 'joana', 'andria', 'alice'];
+
+nomesDeEstudantes.push('pedro');//Coloca o nome indicado ao fim da lista array, ou seja incluí um ítem ao final
+console.log(nomesDeEstudantes);
+
+nomesDeEstudantes.pop(); //Retira o último nome da lista array, ou seja excluí o último itém
+console.log(nomesDeEstudantes);
---
+++ Módulo01/Aula03-Array-Loop/Shift-unshift.js
@@ -0,0 +1,7 @@
+const nomesDeEstudantes = ['jose', 'joao', 'maria', 'joana'];
+
+nomesDeEstudantes.shift(); // Remove o primeiro elemento de um ARRAY
+console.log(nomesDeEstudantes);
+
+nomesDeEstudantes.unshift('igor'); // Adiciona um ítem sempre no ínicio de uma lista array.
+console.log(nomesDeEstudantes);
