4cc7267fd803949b3c384070d02f6276a3df06d1
IgorALves <igorianrgit@gmail.com>
2024-01-31 13:43:33 -0300

    Exercício de Condicionais

---
+++ Módulo01/Exercícios de Aula/Exercicio-de-condicionais/Peneira-volei.js
@@ -0,0 +1,7 @@
+const alturaEmCm = 179
+
+if (alturaEmCm >= 180) {
+    console.log("APROVADO para a segunda fase da peneira")
+} else {
+    console.log("REPROVADO para as seguintes fases da peneira")
+}
