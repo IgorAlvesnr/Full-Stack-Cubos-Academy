65024b06eebef6a2df84be72fab0b5e74951cfdf
IgorALves <igorianrgit@gmail.com>
2024-01-31 17:43:45 -0300

    Exercício de Condicionais

---
+++ Módulo01/Exercícios de Aula/Exercicio-de-condicionais/Identificar-caractere.js
@@ -0,0 +1,13 @@
+const caractere = "A";
+
+if ("aeiou".includes(caractere.toLowerCase())) {
+    console.log(`O caractere "${caractere}" é uma VOGAL MINÚSCULA`);
+} else if ("AEIOU".includes(caractere.toUpperCase())) {
+    console.log(`O caractere "${caractere}" é uma VOGAL MAIÚSCULA`);
+} else if ("bcdfghjklmnpqrstvwxyz".includes(caractere.toLowerCase())) {
+    console.log(`O caractere "${caractere}" é uma CONSOANTE MINÚSCULA`);
+} else if ("BCDFGHJKLMNPQRSTVWXYZ".includes(caractere.toUpperCase())) {
+    console.log(`O caractere "${caractere}" é uma CONSOANTE MAIÚSCULA`)
+} else {
+    console.log(`O caractere "${caractere}" é um NUMERAL`)
+}
