963c0bcd3b7262de2241cddbbf56d30cf96679fc
IgorALves <igorianrgit@gmail.com>
2024-01-31 19:31:42 -0300

    Aula de ARRAY e LOOPs

--- Módulo01/Aula03-Array-Loop/Introdução.js
+++ Módulo01/Aula03-Array-Loop/Introdução.js
@@ -7,3 +7,12 @@ console.log(nomeDeEstudantes[2]);//maria
 console.log(nomeDeEstudantes[3]);//joana
 console.log(nomeDeEstudantes[4]);//andre
 console.log(nomeDeEstudantes[5]);//undefined pq não tem nome no index 5.
+
+//Para MUDAR algum nome que está dentro do ARRAY é só utilizar o exemplo abaixo
+nomeDeEstudantes[0] = 'Igor';
+console.log(nomeDeEstudantes);
+//o numero dentro dos colchetes[ ], escolhe a posição da alteração.
+
+const x = 3;
+nomeDeEstudantes[x] = 'Andreia';
+console.log(nomeDeEstudantes);
