19dbe9ad2e912c29fcb725b8c41aff723cd032e1
IgorALves <igorianrgit@gmail.com>
2024-02-02 01:01:10 -0300

    Exercícios de Condicionais

---
+++ Módulo01/Exercícios de Aula/Exercicio-de-condicionais/Nota-conceito.js
@@ -0,0 +1,13 @@
+const nota = 3.5;
+
+if (nota >= 9 && nota <= 10) {
+    console.log('O Aluno tirou nota "A".')
+} else if (nota >= 8 && nota <= 8.9) {
+    console.log('O Aluno tirou nota "B".')
+} else if (nota >= 6 && nota <= 7.9) {
+    console.log('O Aluno tirou "C".')
+} else if (nota >= 4 && nota <= 5.9) {
+    console.log('O Aluno tirou "D".')
+} else {
+    console.log('O Aluno tirou "E".')
+}
