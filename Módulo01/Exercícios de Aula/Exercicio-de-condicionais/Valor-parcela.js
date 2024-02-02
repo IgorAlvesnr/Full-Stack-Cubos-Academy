0d0dd711b88a077a6085e915da6fc83ebca278c6
IgorALves <igorianrgit@gmail.com>
2024-02-02 02:43:37 -0300

    Exercício de Condicionais

--- Módulo01/Exercícios de Aula/Exercicio-de-condicionais/Valor-parcela.js
+++ Módulo01/Exercícios de Aula/Exercicio-de-condicionais/Valor-parcela.js
@@ -1,4 +1,4 @@
-const rendaMensal = 3000;
+const rendaMensal = 2500;
 const valorCurso = 18000;
 const percentualParcela = 0.18;
 const duracaoContratoMeses = 60;
@@ -11,12 +11,15 @@ if (tempoDecorrido <= duracaoContratoMeses && rendaMensal >= 2000) {
     valorParcela = rendaMensal * percentualParcela;
     somaParcelas = valorParcela * tempoDecorrido;
     tempoDecorrido++;
+    console.log(`a) O valor da sua parcela é R$ ${valorParcela.toFixed(2)}`);
+    console.log(`b) Tempo decorrido de contrato: ${tempoDecorrido} meses`);
+    console.log(`c) Soma das parcelas já pagas: R$ ${(somaParcelas).toFixed(2)}`);
 }
 
-console.log(`a) O valor da sua parcela é R$ ${valorParcela.toFixed(2)}`);
-console.log(`b) Tempo decorrido de contrato: ${tempoDecorrido} meses`);
-console.log(`c) Soma das parcelas já pagas: R$ ${(somaParcelas).toFixed(2)}`);
+let valorQueFalta = valorCurso - somaParcelas
 
-if (somaParcelas === valorCurso * 100) {
-    console.log("Parabéns! Você quitou a dívida. Não deve mais nada.");
+if (somaParcelas >= valorCurso) {
+    console.log('PARABÉNS! Você quitou a divida, não deve mais nada.')
+} else {
+    console.log(`Ainda há divida a ser quitada. Um total de ${valorQueFalta}`)
 }
