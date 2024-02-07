c51532c8a025b28343c4de8d400cc6237ed6e91c
IgorALves <igorianrgit@gmail.com>
2024-02-07 19:26:24 -0300

    Aula de Objetos

--- Módulo01/Aula04-Objetos/01-Criando-Objetos.js
+++ Módulo01/Aula04-Objetos/01-Criando-Objetos.js
@@ -6,9 +6,10 @@ const pessoa = {
 console.log(pessoa);
 
 pessoa.nome = "Igor"
+pessoa['altura'] = 1.80
 
 console.log(pessoa.nome);
-console.log(`Meu nome é ${pessoa.nome}, tenho ${pessoa.altura}m e estou pesando ${pessoa.peso}Kg.`)
+console.log(`Meu nome é ${pessoa.nome}, tenho ${pessoa.altura} m e estou pesando ${pessoa.peso}Kg.`)
 
 /* Na Aula "Criando e manipulando Objetos" foram abordados os seguintes tópicos:
 
@@ -16,11 +17,14 @@ console.log(`Meu nome é ${pessoa.nome}, tenho ${pessoa.altura}m e estou pesando
 2. Explicação de como as propriedades de um objeto funcionam, destacando que são separadas por vírgulas e podem 
 armazenar diferentes tipos de dados, como strings, números e outros.
 3.Utilização da notação de ponto (objeto.propriedade) para acessar o valor de uma propriedade específica do objeto.
-4.Demonstração do uso de template string (uso de crases) para facilitar a concatenação de valores das propriedades 
+4.Foi abordada a manipulação de objetos em JavaScript, enfatizando o uso da notação de colchetes para acessar 
+propriedades de um objeto. Foi destacado que essa notação funciona de forma equivalente à notação de ponto, 
+permitindo que as propriedades sejam acessadas e modificadas da mesma maneira.
+5.Demonstração do uso de template string (uso de crases) para facilitar a concatenação de valores das propriedades 
 com outras strings.
-5.Esclarecimento de que, apesar de ser uma constante (const), é possível modificar o conteúdo das propriedades 
+6.Esclarecimento de que, apesar de ser uma constante (const), é possível modificar o conteúdo das propriedades 
 do objeto.
-6.Diferença entre modificar uma propriedade do objeto e atribuir um novo objeto à variável, ressaltando que a 
+7.Diferença entre modificar uma propriedade do objeto e atribuir um novo objeto à variável, ressaltando que a 
 reatribuição do objeto não é permitida quando declarado com const. Caso fosse uma variável let seria possível 
 alterar.
 
