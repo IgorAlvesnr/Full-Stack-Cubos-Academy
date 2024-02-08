f9300aa8c6f809a98f6e949f55dd854578fb2d18
IgorALves <igorianrgit@gmail.com>
2024-02-08 20:00:50 -0300

    Exercícios de Condicionais

---
+++ Módulo01/Exercícios de Aula/Exercicio-de-condicionais/Extrato-compra.js
@@ -0,0 +1,5 @@
+const valorDoProduto = 300;
+const quantidadeDaParcela = 5;
+
+const resultado = (valorDoProduto / quantidadeDaParcela).toFixed(2);
+console.log(`Restam ${quantidadeDaParcela} parcelas de R$ ${resultado} reais`)
