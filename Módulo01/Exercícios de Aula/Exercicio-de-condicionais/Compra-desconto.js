f99026067609d23f67e778990b0174ce3118bbb4
IgorALves <igorianrgit@gmail.com>
2024-02-08 19:24:19 -0300

    Exercícios de Condicionais

--- Módulo01/Exercícios de Aula/Exercicio-de-condicionais/Compra-desconto.js
+++ Módulo01/Exercícios de Aula/Exercicio-de-condicionais/Compra-desconto.js
@@ -1,6 +1,16 @@
-const credito = 0.05;
-const cheque = 0.03;
-const debitoDinheiro = 0.1;
+const credito = 0.95;
+const cheque = 0.97;
+const debitoDinheiro = 0.9;
 const valorProduto = 100
+let resultado = debitoDinheiro
 
-if (cred)
+if (resultado === credito) {
+    resultado = valorProduto * credito;
+    console.log(`O valor do produto é R$ ${resultado} reais, por conta do acrescimo na modalidade CRÉDITO`);
+} else if (resultado === cheque) {
+    resultado = valorProduto * cheque;
+    console.log(`O valor do produto é R$ ${resultado} reais, por conta do acrescimo na modalidade CHEQUE`)
+} else {
+    resultado = valorProduto * debitoDinheiro;
+    console.log(`O valor do produto é R$ ${resultado} reais, por conta do acrescimo na modalidade DÉBITO/DINHEIRO`)
+}
