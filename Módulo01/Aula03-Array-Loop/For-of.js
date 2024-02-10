6f612d4e76d2ff82776ec8eef231c4d0d6f094dc
Igor Alves <157431285+IgorAlvesnr@users.noreply.github.com>
2024-02-10 01:36:31 -0300

    Update For-of.js
    Aula de Array/Loops

--- Módulo01/Aula03-Array-Loop/For-of.js
+++ Módulo01/Aula03-Array-Loop/For-of.js
@@ -3,3 +3,7 @@ const nomes = ['jose', 'joao', 'maria', 'joana', 'andre']
 for (let nome of nomes) {
     console.log(nome)
 }
+
+/* conceito: for(let item of array){
+    o código entre as chaves se repetirá uma vez para cada item do array
+}  */
