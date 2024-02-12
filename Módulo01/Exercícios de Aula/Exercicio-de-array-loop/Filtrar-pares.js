935591dfa62cdfedeadc188a6064f88c78ea6513
IgorALves <igorianrgit@gmail.com>
2024-02-12 07:01:02 -0300

    Exercícios de ARRAY/LOOPs

--- Módulo01/Exercícios de Aula/Exercicio-de-array-loop/Filtrar-pares.js
+++ Módulo01/Exercícios de Aula/Exercicio-de-array-loop/Filtrar-pares.js
@@ -1,6 +1,4 @@
 const numeros = [1, 4, 12, 21, 53, 88, 112];
 
-const pares = numeros.filter((value) => {
-    return value % 2 === 0;
-})
+const pares = numeros.filter(numero => numero % 2 === 0);
 console.log("Este ARRAY é só de números pares:", pares);
---
+++ Módulo01/Exercícios de Aula/Exercicio-de-array-loop/Separar-pares-impares.js
@@ -0,0 +1,11 @@
+const original = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+const pares = original.filter(numero => {
+    return numero % 2 === 0;
+})
+console.log("Este ARRAY é só de números pares:", pares);
+
+const impares = original.filter(numero => {
+    return numero % 2 === 1;
+})
+console.log("Este ARRAY é só de números impares:", impares);
