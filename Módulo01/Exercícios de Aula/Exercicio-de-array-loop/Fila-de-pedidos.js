033097dedfc7fcd0417525f76d111349c38b3df1
IgorALves <igorianrgit@gmail.com>
2024-02-12 05:04:02 -0300

    Exercício de ARRAY/LOOPs

---
+++ Módulo01/Exercícios de Aula/Exercicio-de-array-loop/Fila-de-pedidos.js
@@ -0,0 +1,6 @@
+const filaDePedidos = ["pedido 12", "pedido 13", "pedido 14"];
+
+filaDePedidos.push("pedido 15"); //Adiciona ao final
+filaDePedidos.shift(); //remove o primeiro
+
+console.log("A fila de pedidos ATUAL:", filaDePedidos);
