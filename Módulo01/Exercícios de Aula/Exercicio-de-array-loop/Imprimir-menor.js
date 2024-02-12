ea87677c8e713070b757125a3133424c05a99378
IgorALves <igorianrgit@gmail.com>
2024-02-12 06:48:36 -0300

    Exercício de ARRAY/LOOPs

--- Módulo01/Exercícios de Aula/Exercicio-de-array-loop/Imprimir-menor.js
+++ Módulo01/Exercícios de Aula/Exercicio-de-array-loop/Imprimir-menor.js
@@ -1,7 +1,7 @@
 const arrayA = [5, 32, 3, 44, 1];
 const arrayB = [57, 4, 21, 2, 13];
 
-const arrayConcatenado = arrayA.concat(arrayB);
-const menorNumero = Math.min(...arrayConcatenado);
+const novoArray = arrayA.concat(arrayB);
+const menorNumero = Math.min(...novoArray);
 
 console.log("O menor número dentre os dois ARRAY's é:", menorNumero)
