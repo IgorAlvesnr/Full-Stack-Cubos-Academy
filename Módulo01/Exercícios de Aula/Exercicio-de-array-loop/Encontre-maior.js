2d9df407340e6e827565c72e58510138cef02f24
IgorALves <igorianrgit@gmail.com>
2024-02-12 04:54:04 -0300

    Exercício de ARRAY/LOOPs

---
+++ Módulo01/Exercícios de Aula/Exercicio-de-array-loop/Encontre-maior.js
@@ -0,0 +1,6 @@
+const numeros = [3, 4, 1, 8, 11, 7, 5];
+const maiorNumero = Math.max(...numeros);//Math.max retorna o maior numero dentro do array
+
+console.log("O maior número deste array é:", maiorNumero);
+
+//Math.min retorna o menor numero de um array
