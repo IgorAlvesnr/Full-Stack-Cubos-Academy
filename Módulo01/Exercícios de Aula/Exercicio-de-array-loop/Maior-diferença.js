49e6586271bc29f7a8def2c5e1249ec29b7ef91a
IgorALves <igorianrgit@gmail.com>
2024-02-12 05:09:04 -0300

    Exercício de ARRAY/LOOPs

---
+++ Módulo01/Exercícios de Aula/Exercicio-de-array-loop/Maior-diferença.js
@@ -0,0 +1,6 @@
+const numeros = [8, 11, 4, 1];
+const maiorNumero = Math.max(...numeros);
+const menorNumero = Math.min(...numeros);
+
+const diferenca = maiorNumero - menorNumero;
+console.log(diferenca)
