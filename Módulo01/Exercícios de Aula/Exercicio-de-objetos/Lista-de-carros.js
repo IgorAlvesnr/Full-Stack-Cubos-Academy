6b03e1265d6c03e2d908032c212d1e225cd60c30
IgorALves <igorianrgit@gmail.com>
2024-02-17 12:48:23 -0300

    Exercícios de Objetos

---
+++ Módulo01/Exercícios de Aula/Exercicio-de-objetos/Lista-de-carros.js
@@ -0,0 +1,25 @@
+const carros = [{
+    marca: "Volkswagen",
+    modelo: "Gol",
+    ano: 2022,
+    cor: "vermelho",
+    quantidade_portas: 4,
+    automatico: false
+}, {
+    marca: "GM",
+    modelo: "Cobalt",
+    ano: 2017,
+    cor: "Branco",
+    quantidade_portas: 4,
+    automatico: true
+
+}, {
+    marca: "Honda",
+    modelo: "HRV",
+    ano: 2020,
+    cor: "preto",
+    quantidade_portas: 4,
+    automatico: true
+
+}]
+console.log(carros[2]);
