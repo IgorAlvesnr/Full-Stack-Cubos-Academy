34f46ac14f760096bcffa356e9cc64489249b188
IgorALves <igorianrgit@gmail.com>
2024-02-17 12:53:48 -0300

    Exercícios de Objetos

---
+++ Módulo01/Exercícios de Aula/Exercicio-de-objetos/Mensagem-apresentacao.js
@@ -0,0 +1,7 @@
+const usuario = {
+    nome: "Igor Alves",
+    idade: 32,
+    profissao: "programador",
+    cor_preferida: "Azul",
+}
+console.log(`Olá, meu nome é ${usuario.nome}, tenho ${usuario.idade} anos, sou ${usuario.profissao} e minha cor preferida é ${usuario.cor_preferida}!`)
