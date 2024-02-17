88e00181327584d46721a534cc987297da5be52b
IgorALves <igorianrgit@gmail.com>
2024-02-17 13:35:30 -0300

    Exercícios de Objetos

---
+++ Módulo01/Exercícios de Aula/Exercicio-de-objetos/Cadastro-de-aulas.js
@@ -0,0 +1,26 @@
+const curso = {
+    id: 1234,
+    nome: "Lógica de programação",
+    aulas: [],
+};
+
+const aula = [{
+    identificador: 1,
+    nomeDaAula: "Lógica de Programação"
+}, {
+    identificador: 2,
+    nomeDaAula: "Variáveis"
+}, {
+    identificador: 3,
+    nomeDaAula: "Condicionais"
+}, {
+    identificador: 4,
+    nomeDaAula: "Arrays"
+}
+];
+
+aula.forEach(juntar => {
+    curso.aulas.push(juntar)
+});
+
+console.log(curso);
