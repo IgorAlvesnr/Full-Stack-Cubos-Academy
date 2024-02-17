2bb61f9948d07e469fa788a361338601b87a9195
IgorALves <igorianrgit@gmail.com>
2024-02-17 12:34:49 -0300

    Exercício de Objetos

---
+++ Módulo01/Exercícios de Aula/Exercicio-de-objetos/Cadastro-de-usuarios.js
@@ -0,0 +1,19 @@
+const endereco = {
+    rua: "Duarte Albuquerque",
+    numero: 146,
+    complemento: "fundos",
+    bairro: "Realengo",
+    cep: 21111321,
+    cidade: "Rio de janeiro",
+    estado: "RJ",
+    pais: "Brasil"
+};
+
+const usuario = {
+    nome: "Igor Alves",
+    email: "igor@gmail.com",
+    telefone: 21999999999,
+    dataDeNascimento: "06021992",
+    endereco
+};
+console.log(usuario);
