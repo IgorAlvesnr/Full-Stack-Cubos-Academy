74449791273f27940bf3952a2b273d582aaa6ad2
IgorALves <igorianrgit@gmail.com>
2024-02-19 19:19:03 -0300

    Aula de Funções

---
+++ Módulo01/Aula05-Funções/02-Funcao-com-parametros.js
@@ -0,0 +1,8 @@
+function cumprimentar(pessoa, idade) {
+    console.log(`Olá! ${pessoa}! Fiquei sabendo que vc tem ${idade} anos.`)
+}
+
+cumprimentar("José", 30);
+cumprimentar("João", 20);
+cumprimentar("Ana", 40);
+cumprimentar("Maria", 15);
