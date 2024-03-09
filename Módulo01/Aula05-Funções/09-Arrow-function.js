7f4baa5605fd4c90a9e2ab5f8809f67f2fa2a6d5
IgorALves <igorianrgit@gmail.com>
2024-03-09 01:55:30 -0300

    Aula de Funções

---
+++ Módulo01/Aula05-Funções/09-Arrow-function.js
@@ -0,0 +1,13 @@
+// Declaração de função TRADICIONAL
+function soma(x, y) {
+    //código
+    const resultado = x + y;
+    return resultado;
+}
+
+//Declaração de função ARROW-FUNCTION
+const soma = (x, y) => {
+    //código
+    const resultado = x + y;
+    return resultado;
+}
