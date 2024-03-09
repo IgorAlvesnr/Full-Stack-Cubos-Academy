21ffc34234f7becfcd06a4e299b2702adfef5bb1
IgorALves <igorianrgit@gmail.com>
2024-03-09 02:22:07 -0300

    Aula de Funções

--- Módulo01/Aula05-Funções/10-Metodo.js
+++ Módulo01/Aula05-Funções/10-Metodo.js
@@ -1,20 +1,19 @@
-function determinarFaixaEtaria(idade) {
-    if (idade <= 21) {
-        return "Jovem"
-    } else if (idade < 66) {
-        return "Adulto(a)"
-    } else {
-        return "Idoso(a)"
-    }
-}
-
 const pessoa1 = {
     nome: "José",
     idade: 30,
     profissao: "professor",
     apresentar: function () {
-        const faixaEtaria = determinarFaixaEtaria(this.idade);
+        const faixaEtaria = this.determinarFaixaEtaria(this.idade);
         console.log(`Sou ${this.nome}, sou um(a) ${faixaEtaria} de ${this.idade} anos e sou ${this.profissao}.`)
+    },
+    determinarFaixaEtaria: function () {
+        if (this.idade <= 21) {
+            return "Jovem"
+        } else if (this.idade < 66) {
+            return "Adulto(a)"
+        } else {
+            return "Idoso(a)"
+        }
     }
 }
 
