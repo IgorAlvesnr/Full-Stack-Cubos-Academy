66d5f7e5bb31d6031c67b725e52c2546ce766f3f
IgorALves <igorianrgit@gmail.com>
2024-03-09 01:46:20 -0300

    Aula de Funções

---
+++ Módulo01/Aula05-Funções/08-Escopo.js
@@ -0,0 +1,10 @@
+let z = 5;
+
+function soma(x, y) {
+    return x + y + z;
+}
+
+console.log(soma(5, 3));
+
+// O que está dentro do escopo pode ser usado dentro mas não pode ser usado fora.
+// já o que está fora do escopo pode ser usado dentro e fora.
