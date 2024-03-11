6ed6c6f4578d047794bf87f97865000934338eba
IgorALves <igorianrgit@gmail.com>
2024-03-11 15:55:32 -0300

    Aula de Métodos de Strings

---
+++ Módulo01/Aula06-Metodos-Strings/07-Exercicio01.js
@@ -0,0 +1,8 @@
+const numero = "97.50";
+
+const variavelAlterada = numero.replace(".", ",");
+
+console.log(numero);
+console.log(variavelAlterada);
+
+/* Muito importante lembrando que tem que ser uma variável do tipo string. mesmo se for números. */
