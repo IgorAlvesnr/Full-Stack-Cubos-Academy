c034de190ab403bc28a9aba1613cb042727889bb
IgorALves <igorianrgit@gmail.com>
2024-03-11 15:08:53 -0300

    Aula de Métodos de Strings

---
+++ Módulo01/Aula06-Metodos-Strings/02-Includes.js
@@ -0,0 +1,17 @@
+const frase = "Eu estou aprendendo a programar na Cubos Academy";
+
+console.log(frase.includes("Cubos"));
+
+/*
+Método includes():
+Utilização do método includes() para verificar se a palavra "Cubos" está presente na frase
+Retorno da função includes()
+
+Exemplos de uso
+Impressão do retorno da função includes() no console
+Uso do parâmetro opcional para indicar a posição inicial da busca
+
+Observações
+Importância da igualdade entre strings (maiúsculas e minúsculas)
+Utilização da função console.log() para imprimir o resultado da busca
+*/
