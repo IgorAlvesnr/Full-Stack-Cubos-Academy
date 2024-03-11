d5e3b16642e4de88497c7826d3578cc50b9dec63
IgorALves <igorianrgit@gmail.com>
2024-03-11 14:52:09 -0300

    Aula de Métodos de Strings

---
+++ Módulo01/Aula06-Metodos-Strings/01-Introducao.js
@@ -0,0 +1,21 @@
+const texto = "Cubos Academy";
+
+console.log(texto.length);
+
+/*
+Esta aula aborda os métodos mais comuns utilizados em strings em JavaScript. Durante a aula, você aprenderá como
+utilizar propriedades e métodos para manipular e transformar strings. Além disso, você aprenderá sobre conversões 
+de strings em objetos e utilizará exemplos de uso comum para fixar o conteúdo. Se você deseja aprimorar suas 
+habilidades em manipulação de strings em JavaScript, essa aula será extremamente útil. Venha participar e fortalecer 
+seus conhecimentos nesta área!
+
+Introdução aos métodos de strings em JavaScript
+Propriedade "length" - retorna o tamanho da string
+Método "charAt(index)" - retorna o caractere na posição especificada pelo índice
+Método "toUpperCase()" - retorna a string em letras maiúsculas
+Método "toLowerCase()" - retorna a string em letras minúsculas
+Método "indexOf(substring)" - retorna o índice da primeira ocorrência da substring na string
+Método "lastIndexOf(substring)" - retorna o índice da última ocorrência da substring na string
+Método "replace(oldSubstring, newSubstring)" - substitui todas as ocorrências da oldSubstring pela newSubstring na string
+Método "split(delimiter)" - divide a string em um array de substrings com base no delimitador informado
+Método "trim()" - remove os espaços em branco do início e do fim da string. */
