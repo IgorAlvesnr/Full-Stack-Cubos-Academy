535811d04c9d9a06a7d33124083419ae50dbcb4f
IgorALves <igorianrgit@gmail.com>
2024-03-11 19:20:32 -0300

    Aula de ARRAY/LOOPs

---
+++ Módulo01/Aula03-Array-Loop/Includes.js
@@ -0,0 +1,5 @@
+const lista = ["Eu", "Tu", "Eles"];
+const boolean = lista.includes("Tu");
+console.log(boolean);
+
+// Verifica se exite um item dentro de um array, retornando true ou false.
---
+++ Módulo01/Aula03-Array-Loop/IndexOf.js
@@ -0,0 +1,5 @@
+const lista = ["Eu", "Tu", "Eles"];
+const indice = lista.indexOf("Tu");
+console.log(indice);
+
+// Encontra um item dentro de um array, retornando o índice da primeira ocoreência do item procurado.
