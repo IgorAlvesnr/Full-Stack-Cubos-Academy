906a92072c423fa86480bdbe2be057f7e1665198
IgorALves <igorianrgit@gmail.com>
2024-03-11 16:20:33 -0300

    Aula de Métodos de Strings

---
+++ Módulo01/Aula06-Metodos-Strings/09-toUpperCase-&-toLowerCase.js
@@ -0,0 +1,9 @@
+const texto = "Eu amo a Cubos Academy, amo muito"
+
+const gritando = texto.toUpperCase();
+const reduzindo = gritando.toLowerCase();
+console.log(gritando);
+console.log(reduzindo);
+
+
+// Coloca todas as letras de uma determinada string em maíusculas.
