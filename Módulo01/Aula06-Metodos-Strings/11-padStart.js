17c1192c0be47793957c08b16de580cc962817b7
IgorALves <igorianrgit@gmail.com>
2024-03-11 16:35:50 -0300

    Aula de Métodos de Strings

---
+++ Módulo01/Aula06-Metodos-Strings/11-padStart.js
@@ -0,0 +1,10 @@
+const ultimos4Dig = "2345";
+
+const numCartao = ultimos4Dig.padStart(19, "**** ");
+
+console.log(numCartao);
+
+/* 
+Preenche uma string, colocando text no início dela (várias vezes, se necessário) até que a String atinja
+o tamanho lenght. 
+*/
