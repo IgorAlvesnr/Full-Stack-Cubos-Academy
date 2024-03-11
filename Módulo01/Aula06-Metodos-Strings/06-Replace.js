6f1a8d9b48261fee6245ce1608de590592eb8730
IgorALves <igorianrgit@gmail.com>
2024-03-11 15:49:20 -0300

    Aula de Métodos de Strings

---
+++ Módulo01/Aula06-Metodos-Strings/06-Replace.js
@@ -0,0 +1,11 @@
+const texto = "Eu amo a Cubos Academy, amo muito";
+
+const parte = texto.replace("amo", "adoro");
+
+console.log(parte);
+
+/* 
+replace("text", "newText");
+
+O método procura por text numa string e substitui a primeira ocorrência por um newText, ao encontrar.
+*/
