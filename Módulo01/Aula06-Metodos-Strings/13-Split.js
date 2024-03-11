c1a591881aea91b7cb6d75708c7f065637cf9a02
IgorALves <igorianrgit@gmail.com>
2024-03-11 16:57:24 -0300

    Aula de Métodos de Strings

---
+++ Módulo01/Aula06-Metodos-Strings/13-Split.js
@@ -0,0 +1,8 @@
+const nomeCompleto = "José Messias Pereira";
+const array = nomeCompleto.split(" ");
+const nomeDoMeio = array[1];
+
+console.log(nomeDoMeio);
+
+//Transforma uma string num array de Strings menores.
+//Seprando-a em todos os lugares onde encontrar o argumento text.
