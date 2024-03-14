9fd5bbaa71fe08cd82dd5928206a49bd62914d1a
IgorALves <igorianrgit@gmail.com>
2024-03-14 18:32:07 -0300

    Desafio do módulo01

--- Módulo01/Desafio-01/08-Americano.js
+++ Módulo01/Desafio-01/08-Americano.js
@@ -1,25 +1,23 @@
 function encontrarGoleiro(numerosJogados) {
-    const S = numerosJogados.reduce((total, numero) => total + numero, 0); // Somando todos os números jogados
-    const posicaoGoleiro = S % numerosJogados.length; // Encontrando a posição do goleiro usando o módulo
-
-    // Se a posição for 0, o último jogador será o goleiro
+    const S = numerosJogados.reduce((total, numero) => total + numero, 0);
+    const posicaoGoleiro = S % numerosJogados.length;
     if (posicaoGoleiro === 0) {
         return {
             posicao: numerosJogados.length,
-            indice: numerosJogados.length - 1 // Índice do último jogador no array
+            indice: numerosJogados.length - 1
         };
     } else {
         return {
             posicao: posicaoGoleiro,
-            indice: posicaoGoleiro - 1 // Índice do jogador correspondente no array
+            indice: posicaoGoleiro - 1
         };
     }
 }
 
-// Teste com os valores fornecidos
-const numerosJogados = [1, 3, 2, 1]; // Exemplo de entrada
+const numerosJogados = [1, 3, 2, 1];
 const goleiro = encontrarGoleiro(numerosJogados);
-const totalJogadores = numerosJogados.reduce((total, numero) => { return total + numero; }, 0)
+const totalJogadores = numerosJogados.reduce((total, numero) => { return total + numero; }, 0);
+
 console.log("A soma total jogado pelos jogadores é: ", totalJogadores);
 console.log("Posição do goleiro:", goleiro.posicao);
 console.log("Índice do goleiro no array:", goleiro.indice);
