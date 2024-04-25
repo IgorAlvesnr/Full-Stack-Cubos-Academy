f93780a58138b972d5ecd818370d0576c0ac1bde
IgorALves <igorianrgit@gmail.com>
2024-04-25 19:38:53 -0300

    Como usar API express em funções Assíncrona

--- Módulo02/Aula-005-Programação-Assincrona/04-API-express-funcoes-assincronas/src/index.js
+++ Módulo02/Aula-005-Programação-Assincrona/04-API-express-funcoes-assincronas/src/index.js
@@ -4,8 +4,16 @@ const { getCityFromZipcode } = require('utils-playground')
 const app = express();
 
 app.get('/', async (req, res) => {
-    const cidade = await getCityFromZipcode('21730560');
-    res.send(`A cidade encontrada foi: ${cidade}`);
+    const cidade = getCityFromZipcode('21730560');
+    const cidade2 = getCityFromZipcode('41256250');
+
+    const promise = await Promise.all([cidade, cidade2]); // promise all resolve todas as promessa de uma vez só
+
+    // console.log(promise);
+
+    const [resposta1, resposta2] = promise;
+
+    res.send(`A cidade encontrada foi: ${resposta1} e ${resposta2}.`);
 });
 
 app.listen(3000);
